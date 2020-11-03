a0fec19b7e17c90019057f8b66efec22cdb21c16
Artem-Potapov <potapov23500@gmail.com>
2020-11-03 12:02:33 +0300

    add pages

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <a-layout id="app" style="min-height: 100%">
     <a-layout-sider
-      v-model="collapsed"
+      :value="false"
       :trigger="null"
       collapsible
       :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }"
     >
       <div class="logo" />
       <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
-        <a-menu-item key="1">
+        <a-menu-item key="1" @click="$router.history.current.path!='/' && $router.push('/')">
           <a-icon type="book" />
           <span>Books</span>
         </a-menu-item>
-        <a-menu-item key="2">
+        <a-menu-item key="2" @click="$router.history.current.path!='/about' && $router.push('/about')">
           <a-icon type="user" />
           <span>Profile</span>
         </a-menu-item>
-        <a-menu-item key="3">
+        <a-menu-item key="3" @click="$router.history.current.path!='/favorite' && $router.push('/favorite')">
           <a-icon type="star" />
           <span>Your favourites</span>
         </a-menu-item>
@@ -24,20 +24,9 @@
     </a-layout-sider>
 
     <a-layout style="margin-left: 200px">
-      <a-layout-content style="flex-grow: 1; padding: 30px; display: flex">
+      <a-layout-content style="flex-grow: 1; padding: 30px">
         <router-view/>
-        <a-card hoverable style="width: 200px; margin-left: 20px; margin-right: 20px; height:450px" v-for="book in books" :key='book.name' >
-          <img
-            slot="cover"
-            alt="example"
-            :src="book.img"
-          />
-          <a-card-meta :title="book.name">
-            <template slot="description">{{book.author}}</template>
-          </a-card-meta>
-          <a-rate v-model="book.rate" :tooltips="desc" allow-half disabled/>
-          <a-button :href='book.download' type="link" shape="round" icon="download" download='123' size="small">download</a-button>
-        </a-card>
+        
       </a-layout-content>
       <a-layout-footer> Footer </a-layout-footer>
     </a-layout>
@@ -47,44 +36,29 @@
 <script>
 import Vue from "vue";
 
-import { Button, Card, Layout, Menu, Icon, Rate } from "ant-design-vue";
-import "ant-design-vue/lib/button/style/css";
+import {Layout, Menu, Icon} from "ant-design-vue";
 import "ant-design-vue/lib/menu/style/css";
 import "ant-design-vue/lib/layout/style/css";
-import "ant-design-vue/lib/card/style/css";
 import "ant-design-vue/lib/icon/style/css";
-import "ant-design-vue/lib/rate/style/css";
-Vue.use(Button);
 Vue.use(Menu);
-Vue.use(Card);
 Vue.use(Layout);
 Vue.use(Icon);
-Vue.use(Rate);
+
 
 export default {
   name: "App",
-  mounted(){
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'http://localhost:5000')
-    xhr.send()
-    xhr.onload = () => {
-      if (xhr.readyState != 4) return
-      this.books = JSON.parse(xhr.response)
-    }
-  },
+
   components: {},
   data() {
     return {
-      size: 'large',
-      desc: ["1", "2", "3", "4", "5"],
-      books: [],
+
     };
   },
-  methods: {
-      handleSizeChange(e) {
-        this.size = e.target.value;
-      },
-    },
+  // methods: {
+  //     handleSizeChange(e) {
+  //       this.size = e.target.value;
+  //     },
+  //   },
 
 };
 
